37bd37bca012afa1db8ff3c0b8ea99d428da5190
Quang Hung <voquanghung12tin@gmail.com>
2025-05-02 14:48:52 +0700

    First commit

---
+++ LaptopShop/BackEnd/ecomshop/src/main/java/com/project/ecomshop/EcomshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecomshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomshopApplication.class, args);
+	}
+
+}
